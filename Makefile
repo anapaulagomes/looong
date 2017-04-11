b9bea7d3da389900f7f5cf42b1fdff7bcb321526
Ana Gomes <agomes@thoughtworks.com>
2017-04-11 18:17:17 -0300

    Initial files

---
+++ Makefile
@@ -0,0 +1,36 @@
+.PHONY: help clean clean-pyc clean-build list tests test-all coverage docs release sdist
+
+help:
+	@echo "clean-build - remove build artifacts"
+	@echo "clean-pyc - remove Python file artifacts"
+	@echo "lint - check style with flake8"
+	@echo "tests - run tests quickly with the default Python"
+	@echo "test-all - run tests on every Python version with tox"
+	@echo "coverage - check code coverage quickly with the default Python"
+
+clean: clean-build clean-pyc
+
+clean-build:
+	rm -fr build/
+	rm -fr dist/
+	rm -fr *.egg-info
+
+clean-pyc:
+	find . -name '*.pyc' -exec rm -f {} +
+	find . -name '*.pyo' -exec rm -f {} +
+	find . -name '*~' -exec rm -f {} +
+
+lint:
+	flake8 tests
+
+test:
+	pytest tests
+
+test-all:
+	tox
+
+coverage:
+	coverage run --source looong setup.py tests
+	coverage report -m
+	coverage html
+	open htmlcov/index.html
---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='looong',
+    version='0.1.0',
+    description='A Finder of Long Parameter List',
+    author='Ana Paula Gomes',
+    author_email='apgomes88@gmail.com',
+    url='https://github.com/anapaulagomes/looong',
+    packages=[
+        'looong',
+    ],
+    license='MIT',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+)
