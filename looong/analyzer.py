eadc6d2a365e94e7a8280fe2a8e0a6847b8b8070
Ana Paula Gomes <apgomes88@gmail.com>
2017-04-19 20:52:46 -0300

    Return top parameter list

--- looong/analyzer.py
+++ looong/analyzer.py
@@ -1,4 +1,10 @@
+from operator import attrgetter
+
+
 class Analyzer(object):
 
-    def __init__(self, content):
-        self.content = content
+    def __init__(self, method_list):
+        self.method_list = method_list
+
+    def top_parameter_list(self):
+        return max(self.method_list, key=attrgetter('parameters_list'))
--- tests/test_analyzer.py
+++ tests/test_analyzer.py
@@ -1,1 +1,15 @@
 from looong.analyzer import Analyzer
+from looong.method import Method
+
+
+def test_should_return_the_method_with_more_parameters():
+    method_list = [ Method('foo', 'my_python_test_file.py', ['bar', 'other_bar']),
+                    Method('fooo', 'my_python_test_file.py', ['bar']),
+                    Method('foooo', 'my_python_test_file.py', ['bar', 'other_bar', 'bar_plus_bar']),
+                    Method('fooooo', 'my_python_test_file.py', ['bar', 'other_bar'])]
+    analyzer = Analyzer(method_list)
+    method_with_more_parameters = analyzer.top_parameter_list()
+    expected_method_with_more_parameters = Method('foooo', 'my_python_test_file.py', ['bar', 'other_bar', 'bar_plus_bar'])
+
+    assert expected_method_with_more_parameters.name == method_with_more_parameters.name
+    assert len(expected_method_with_more_parameters.parameters_list) == len(method_with_more_parameters.parameters_list)
