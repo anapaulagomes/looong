b4aadd21224701638ea70b7f835a09624eb1b497
Ana Paula Gomes <apgomes88@gmail.com>
2017-04-26 15:32:58 -0300

    Add support to file parser

--- looong/extractor.py
+++ looong/extractor.py
@@ -1,20 +1,35 @@
 from looong.method import Method
+from looong.codefile import CodeFile
 import re
+import os
 
 
 class Extractor(object):
 
-    def __init__(self, filename):
-        self.filename = filename
+    def __init__(self, directory):
+        self.directory = directory
 
-    def methods(self):
+    def code_files(self):
+        full_directory = os.getcwd() + self.directory
+        code_files_list = []
+
+        for dirpath, dirnames, filenames in os.walk(full_directory):
+            for filename in filenames:
+                filename_with_path = dirpath + '/' + filename
+                method_list = self.__methods(filename_with_path)
+                code_file = CodeFile(filename, dirpath, method_list)
+                code_files_list.append(code_file)
+
+        return code_files_list
+
+    def __methods(self, filename):
         method_list = []
-
-        raw_parameters_list = re.findall(r'def ([a-z]*)\((.*)\)', self.filename.read())
+        filename = open(filename)
+        raw_parameters_list = re.findall(r'def ([a-z]*)\((.*)\)', filename.read())
 
         for name, parameters in raw_parameters_list:
             parameters_list = parameters.replace(' ', '').split(',')
-            method = Method(name, self.filename, [] if parameters_list[0] == '' else parameters_list)
+            method = Method(name, filename, [] if parameters_list[0] == '' else parameters_list)
             method_list.append(method)
 
         return method_list
--- tests/test_extractor.py
+++ tests/test_extractor.py
@@ -1,65 +1,66 @@
 from looong.extractor import Extractor
 from looong.method import Method
-from mock import patch
+from looong.codefile import CodeFile
+import os
+import pytest
+import mock
 from mock import mock_open
 
 
-@patch('__main__.open')
-def test_should_return_method_with_one_parameter_when_extract_from_method_with_one_parameter(file_mock):
-    file_mock.read.return_value = 'def foo(bar):'
-    file_mock.name.return_value = 'foo.py'
-
-    extractor = Extractor(file_mock)
-
-    methods = extractor.methods()
-    expected_parameters = ['bar']
-
-    assert methods[0].parameters_list == expected_parameters
+@pytest.fixture()
+def os_mock(mocker):
+    directory = '/tests/fixtures'
+    mocker.patch('os.getcwd', return_value='/home/bla')
+    yield mocker.patch('os.walk', return_value=[(directory, [], ['foo.py'])])
 
 
-@patch('__main__.open')
-def test_should_extract_multiple_parameters_on_method_parameter_list(file_mock):
-    file_mock.read.return_value = 'def foo(bar, other_bar):'
-    file_mock.name.return_value = 'foo.py'
+def test_should_return_method_with_one_parameter_when_extract_from_method_with_one_parameter(mocker, os_mock):
+    extractor = Extractor('/tests/fixtures')
 
-    extractor = Extractor(file_mock)
+    file_mock = mock.mock_open(read_data='def foo(bar):')
+    with mock.patch('builtins.open', file_mock, create=True):
+        code_files = extractor.code_files()
 
-    methods = extractor.methods()
-    expected_parameters = ['bar', 'other_bar']
+    methods = code_files[0].method_list[0].parameters_list
 
-    assert methods[0].parameters_list == expected_parameters
+    assert ['bar'] == methods
 
 
-@patch('__main__.open')
-def test_should_return_a_method_with_an_empty_list_when_there_is_no_parameters_on_method_parameter_list(file_mock):
-    file_mock.read.return_value = 'def foo():'
-    file_mock.name.return_value = 'foo.py'
+def test_should_extract_multiple_parameters_on_method_parameter_list(mocker, os_mock):
+    extractor = Extractor('/tests/fixtures')
 
-    extractor = Extractor(file_mock)
+    file_mock = mock.mock_open(read_data='def foo(bar, other_bar):')
+    with mock.patch('builtins.open', file_mock, create=True):
+        code_files = extractor.code_files()
 
-    methods = extractor.methods()
-    expected_parameters = []
+    methods = code_files[0].method_list
 
-    assert methods[0].parameters_list == expected_parameters
+    assert ['bar', 'other_bar'] == methods[0].parameters_list
+
+
+def test_should_return_a_method_with_an_empty_list_when_there_is_no_parameters_on_method_parameter_list(mocker, os_mock):
+    extractor = Extractor('/tests/fixtures')
+
+    file_mock = mock.mock_open(read_data='def foo():')
+    with mock.patch('builtins.open', file_mock, create=True):
+        code_files = extractor.code_files()
+
+    assert [] == code_files[0].method_list[0].parameters_list
 
 
 def test_should_return_parameters_list_extracted_from_a_file():
-    code_file = open('tests/fixtures/my_python_test_file.py')
+    extractor = Extractor('/tests/fixtures')
+    code_files = extractor.code_files()
 
-    extractor = Extractor(code_file)
+    assert []                   == code_files[0].method_list[0].parameters_list
+    assert ['bar']              == code_files[0].method_list[1].parameters_list
+    assert ['bar', 'other_bar'] == code_files[0].method_list[2].parameters_list
+    assert ['*bar']             == code_files[0].method_list[3].parameters_list
+    assert ['**bar']            == code_files[0].method_list[4].parameters_list
 
-    methods = extractor.methods()
 
-    expected_file_name = 'my_python_test_file.py'
-    expected_methods = [Method('foo', expected_file_name, []),
-    Method('fooo', expected_file_name, ['bar']),
-    Method('foooo', expected_file_name, ['bar', 'other_bar']),
-    Method('fooooo', expected_file_name, ['*bar']),
-    Method('foooooo', expected_file_name, ['**bar']),
-    ]
+def test_should_return_code_file_with_methods_list():
+    extractor = Extractor('/tests/fixtures')
+    code_files = extractor.code_files()
 
-    assert methods[0].parameters_list == expected_methods[0].parameters_list
-    assert methods[1].parameters_list == expected_methods[1].parameters_list
-    assert methods[2].parameters_list == expected_methods[2].parameters_list
-    assert methods[3].parameters_list == expected_methods[3].parameters_list
-    assert methods[4].parameters_list == expected_methods[4].parameters_list
+    assert 'my_python_test_file.py' == code_files[0].filename
