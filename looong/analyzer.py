a32379ab5999397ad5eeb49716a833eafcd10d73
Ana Paula Gomes <apgomes88@gmail.com>
2017-04-30 19:32:43 -0300

    Perform ranking with top 10 long parameter list

--- looong/analyzer.py
+++ looong/analyzer.py
@@ -13,4 +13,4 @@ class Analyzer(object):
     def long_parameter_list_ranking(self):
         methods_with_long_parameter_list = list(filter(self.has_long_parameter_list, self.method_list))
         methods_with_long_parameter_list = sorted(methods_with_long_parameter_list, key=lambda method: len(method.parameters_list), reverse=True)
-        return methods_with_long_parameter_list
+        return methods_with_long_parameter_list[:10]
--- tests/test_analyzer.py
+++ tests/test_analyzer.py
@@ -40,3 +40,29 @@ def test_do_ranking_with_top_long_parameter_list():
     assert ranking[1] == Method('bar', 'my_python_test_file.py', ['bar', 'other_bar', 'other_bar_barz', 'baz', 'bazz'])
     assert ranking[2] == Method('barbar', 'my_python_test_file.py', ['bar', 'other_bar', 'bar_plus_bar', 'other_bar_barz', 'baz'])
     assert ranking[3] == Method('fooo', 'my_python_test_file.py', ['bar', 'other_bar', 'other_bar_barz', 'bazz'])
+
+
+def test_do_ranking_with_top_10_long_parameter_list():
+    method_list = [ Method('foo', 'my_python_test_file.py', ['bar', 'other_bar', 'other_bar_barz']),
+                    Method('fooo', 'my_python_test_file.py', ['bar', 'other_bar', 'other_bar_barz', 'bazz']),
+                    Method('foooo', 'my_python_test_file.py', ['bar', 'other_bar', 'bar_plus_bar', 'other_bar_barz', 'baz', 'bazz']),
+                    Method('fooooo', 'my_python_test_file.py', ['bar', 'other_bar']),
+                    Method('bar', 'my_python_test_file.py', ['bar', 'other_bar', 'other_bar_barz', 'baz', 'bazz']),
+                    Method('barbar', 'my_python_test_file.py', ['bar', 'other_bar', 'bar_plus_bar', 'other_bar_barz', 'baz']),
+                    Method('foooo', 'my_python_test_file.py', ['bar', 'other_bar', 'bar_plus_bar', 'other_bar_barz', 'baz', 'bazz']),
+                    Method('fooooo', 'my_python_test_file.py', ['bar', 'other_bar']),
+                    Method('bar', 'my_python_test_file.py', ['bar', 'other_bar', 'other_bar_barz', 'baz', 'bazz']),
+                    Method('barbar', 'my_python_test_file.py', ['bar', 'other_bar', 'bar_plus_bar', 'other_bar_barz', 'baz']),
+                    Method('foooo', 'my_python_test_file.py', ['bar', 'other_bar', 'bar_plus_bar', 'other_bar_barz', 'baz', 'bazz']),
+                    Method('fooooo', 'my_python_test_file.py', ['bar', 'other_bar']),
+                    Method('bar', 'my_python_test_file.py', ['bar', 'other_bar', 'other_bar_barz', 'baz', 'bazz']),
+                    Method('barbar', 'my_python_test_file.py', ['bar', 'other_bar', 'bar_plus_bar', 'other_bar_barz', 'baz']),
+                    Method('barbar', 'my_python_test_file.py', ['bar', 'other_bar', 'bar_plus_bar', 'other_bar_barz', 'baz']),
+                    Method('foooo', 'my_python_test_file.py', ['bar', 'other_bar', 'bar_plus_bar', 'other_bar_barz', 'baz', 'bazz']),
+                    Method('fooooo', 'my_python_test_file.py', ['bar', 'other_bar']),
+                    Method('bar', 'my_python_test_file.py', ['bar', 'other_bar', 'other_bar_barz', 'baz', 'bazz']),
+                    Method('barbar', 'my_python_test_file.py', ['bar', 'other_bar', 'bar_plus_bar', 'other_bar_barz', 'baz'])]
+    analyzer = Analyzer(method_list)
+    ranking = analyzer.long_parameter_list_ranking()
+
+    assert 10 == len(ranking)
