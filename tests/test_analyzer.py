f265a793c24c045540cde2b86305f49c2cd91da7
Ana Paula Gomes <apgomes88@gmail.com>
2017-04-30 14:36:52 -0300

    Improve test readability

--- tests/test_analyzer.py
+++ tests/test_analyzer.py
@@ -9,7 +9,6 @@ def test_should_return_the_method_with_more_parameters():
                     Method('fooooo', 'my_python_test_file.py', ['bar', 'other_bar'])]
     analyzer = Analyzer(method_list)
     method_with_more_parameters = analyzer.top_parameter_list()
-    expected_method_with_more_parameters = Method('foooo', 'my_python_test_file.py', ['bar', 'other_bar', 'bar_plus_bar'])
 
-    assert expected_method_with_more_parameters.name == method_with_more_parameters.name
-    assert len(expected_method_with_more_parameters.parameters_list) == len(method_with_more_parameters.parameters_list)
+    assert 'foooo' == method_with_more_parameters.name
+    assert 3 == len(method_with_more_parameters.parameters_list)
