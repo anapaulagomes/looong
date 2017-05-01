51fe0b753f481341d88e54c4a4a8a40f34721241
Ana Paula Gomes <apgomes88@gmail.com>
2017-04-30 23:13:52 -0300

    Add test to execute method

--- tests/test_analyzer.py
+++ tests/test_analyzer.py
@@ -1,5 +1,6 @@
 from looong.analyzer import Analyzer
 from looong.method import Method
+import os
 
 
 def test_return_true_when_method_is_detected_with_long_parameter_list():
@@ -66,3 +67,13 @@ def test_do_ranking_with_top_10_long_parameter_list():
     ranking = analyzer.long_parameter_list_ranking()
 
     assert 10 == len(ranking)
+
+
+def test_when_execute_show_method_filename_parameters_and_number_of_parameters(capsys):
+    method_list = [Method('fooo', 'my_python_test_file.py', ['bar', 'other_bar', 'other_bar_barz', 'baz', 'bazz'])]
+    analyzer = Analyzer(method_list)
+    ranking = analyzer.execute()
+    expected_message = '{} [{}] {} \x1b[6;31;40m{}\x1b[0m\n'.format('fooo', 'my_python_test_file.py', ['bar', 'other_bar', 'other_bar_barz', 'baz', 'bazz'], 5)
+
+    out, err = capsys.readouterr()
+    assert expected_message == out
