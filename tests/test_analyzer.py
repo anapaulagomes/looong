fca0f404bbd475f05efbe66ccf62c1b9d5ae7f87
Ana Paula Gomes <apgomes88@gmail.com>
2017-05-02 15:09:03 -0300

    Fix pythonic order to assert stuff

--- tests/test_analyzer.py
+++ tests/test_analyzer.py
@@ -7,14 +7,14 @@ def test_return_true_when_method_is_detected_with_long_parameter_list():
     method = Method('foo', 'my_python_test_file.py', ['self', 'bar', 'other_bar', 'other_bar_barz'])
     analyzer = Analyzer([])
 
-    assert True is analyzer.has_long_parameter_list(method)
+    assert analyzer.has_long_parameter_list(method) is True
 
 
 def test_return_false_when_method_is_detected_with_long_parameter_list():
     method = Method('foo', 'my_python_test_file.py', ['self', 'bar'])
     analyzer = Analyzer([])
 
-    assert False is analyzer.has_long_parameter_list(method)
+    assert analyzer.has_long_parameter_list(method) is False
 
 
 def test_return_same_method_when_ranking_with_one_method_with_long_parameter_list():
@@ -22,8 +22,8 @@ def test_return_same_method_when_ranking_with_one_method_with_long_parameter_lis
     analyzer = Analyzer(method_list)
     ranking = analyzer.long_parameter_list_ranking()
 
-    assert 1 == len(ranking)
-    assert ranking[0] == Method('fooo', 'my_python_test_file.py', ['bar', 'other_bar', 'other_bar_barz', 'baz', 'bazz'])
+    assert len(ranking) == 1
+    assert Method('fooo', 'my_python_test_file.py', ['bar', 'other_bar', 'other_bar_barz', 'baz', 'bazz']) == ranking[0]
 
 
 def test_do_ranking_with_top_long_parameter_list():
@@ -36,11 +36,11 @@ def test_do_ranking_with_top_long_parameter_list():
     analyzer = Analyzer(method_list)
     ranking = analyzer.long_parameter_list_ranking()
 
-    assert 4 == len(ranking)
-    assert ranking[0] == Method('foooo', 'my_python_test_file.py', ['bar', 'other_bar', 'bar_plus_bar', 'other_bar_barz', 'baz', 'bazz'])
-    assert ranking[1] == Method('bar', 'my_python_test_file.py', ['bar', 'other_bar', 'other_bar_barz', 'baz', 'bazz'])
-    assert ranking[2] == Method('barbar', 'my_python_test_file.py', ['bar', 'other_bar', 'bar_plus_bar', 'other_bar_barz', 'baz'])
-    assert ranking[3] == Method('fooo', 'my_python_test_file.py', ['bar', 'other_bar', 'other_bar_barz', 'bazz'])
+    assert len(ranking) == 4
+    assert Method('foooo', 'my_python_test_file.py', ['bar', 'other_bar', 'bar_plus_bar', 'other_bar_barz', 'baz', 'bazz']) == ranking[0]
+    assert Method('bar', 'my_python_test_file.py', ['bar', 'other_bar', 'other_bar_barz', 'baz', 'bazz'])                   == ranking[1]
+    assert Method('barbar', 'my_python_test_file.py', ['bar', 'other_bar', 'bar_plus_bar', 'other_bar_barz', 'baz'])        == ranking[2]
+    assert Method('fooo', 'my_python_test_file.py', ['bar', 'other_bar', 'other_bar_barz', 'bazz'])                         == ranking[3]
 
 
 def test_do_ranking_with_top_10_long_parameter_list():
@@ -66,7 +66,7 @@ def test_do_ranking_with_top_10_long_parameter_list():
     analyzer = Analyzer(method_list)
     ranking = analyzer.long_parameter_list_ranking()
 
-    assert 10 == len(ranking)
+    assert len(ranking) == 10
 
 
 def test_when_execute_show_method_filename_parameters_and_number_of_parameters(capsys):
@@ -76,4 +76,4 @@ def test_when_execute_show_method_filename_parameters_and_number_of_parameters(c
     expected_message = '{} [{}] {} \x1b[6;31;40m{}\x1b[0m\n'.format('fooo', 'my_python_test_file.py', ['bar', 'other_bar', 'other_bar_barz', 'baz', 'bazz'], 5)
 
     out, err = capsys.readouterr()
-    assert expected_message == out
+    assert out == expected_message
